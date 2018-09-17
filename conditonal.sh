e648525220ce262d4327a1a5f399ad6736dfe8bd
Kofi Opoku <43257737+Kofiop@users.noreply.github.com>
2018-09-17 09:37:04 -0400

    Add files via upload

---
+++ conditonal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "What kind of ice cream did you eat: "
+
+read NAME
+
+if [ "$NAME" = "Vanilla" ]
+then
+	echo "Yummy, I know you can't wait to have it again"
+
+else
+	echo " Gross, dont eat it again"
+
+fi
+
+
